737f7e02f429f4d28219bd962becdeeeaa2dfb4b
Kashish Agrawal <72383661+kashish-ag@users.noreply.github.com>
2021-01-03 21:37:00 +0530

    Add files via upload

---
+++ Reuse.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pickle
+import cv2
+import os
+import matplotlib.pyplot as plt
+from os import listdir
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
+
+
+# Load model
+filename = 'plant_disease_classification_model.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+# Load labels
+filename = 'plant_disease_label_transform.pkl'
+image_labels = pickle.load(open(filename, 'rb'))
+
+
+
+# Dimension of resized image
+DEFAULT_IMAGE_SIZE = tuple((256, 256))
+
+def convert_image_to_array(image_dir):
+    try:
+        image = cv2.imread(image_dir)
+        if image is not None:
+            image = cv2.resize(image, DEFAULT_IMAGE_SIZE)   
+            return img_to_array(image)
+        else:
+            return np.array([])
+    except Exception as e:
+        print(f"Error : {e}")
+        return None
+
+def predict_disease(image_path):
+    image_array = convert_image_to_array(image_path)
+    np_image = np.array(image_array, dtype=np.float16) / 225.0
+    np_image = np.expand_dims(np_image,0)
+    plt.imshow(plt.imread(image_path))
+    result = model.predict_classes(np_image)
+    print((image_labels.classes_[result][0]))
+
+
+
+    predict_disease('/content/PlantVillage/val/Corn_(maize)___Northern_Leaf_Blight/028159fc-995e-455a-8d60-6d377580a898___RS_NLB 4023.JPG')
