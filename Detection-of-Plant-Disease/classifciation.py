11166ed9ec4babacc0bce19e0dee7cb7bbebf5a3
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-06-16 10:52:59 +0530

    Updated README, comments to code, graphics
    Updated README, added comments the code, added some graphics to README
    Please review it

---
+++ Detection-of-Plant-Disease/classifciation.py
@@ -0,0 +1,139 @@
+# Import required Libraries
+import numpy as np
+import pickle
+import cv2
+import os
+import matplotlib.pyplot as plt
+from os import listdir
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
+# Dimension of resized image
+DEFAULT_IMAGE_SIZE = tuple((256, 256))
+
+# Number of images used to train the model
+N_IMAGES = 100
+
+# Path to the dataset folder
+root_dir = './PlantVillage'
+
+# Training and Validating the dataset
+train_dir = os.path.join(root_dir, 'train')
+val_dir = os.path.join(root_dir, 'val')
+
+# Convert Image into Array format
+def convert_image_to_array(image_dir):
+    try:
+        image = cv2.imread(image_dir)
+        if image is not None:
+            image = cv2.resize(image, DEFAULT_IMAGE_SIZE)   
+            return img_to_array(image)
+        else:
+            return np.array([])
+    except Exception as e:
+        print(f"Error : {e}")
+        return None
+
+
+        image_list, label_list = [], []
+
+try:
+    print("[INFO] Loading images ...")
+    plant_disease_folder_list = listdir(train_dir)
+
+    for plant_disease_folder in plant_disease_folder_list:
+        print(f"[INFO] Processing {plant_disease_folder} ...")
+        plant_disease_image_list = listdir(f"{train_dir}/{plant_disease_folder}/")
+
+        for image in plant_disease_image_list[:N_IMAGES]:
+            image_directory = f"{train_dir}/{plant_disease_folder}/{image}"
+            if image_directory.endswith(".jpg")==True or image_directory.endswith(".JPG")==True:
+                image_list.append(convert_image_to_array(image_directory))
+                label_list.append(plant_disease_folder)
+
+    print("[INFO] Image loading completed")  
+except Exception as e:
+    print(f"Error : {e}")
+
+# Transform the loaded training image data into numpy array
+np_image_list = np.array(image_list, dtype=np.float16) / 225.0
+print()
+
+# Check the number of images loaded for training
+image_len = len(image_list)
+print(f"Total number of images: {image_len}")
+
+# Check for the accuracy of the model 
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+# Check the validation loss of the model
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+# Plot showing the Training accuracy and Validation accuracy
+plt.plot(epochs, acc, 'b', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# Plot showing the Training loss  and Validation loss
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and Validation loss')
+plt.legend()
+plt.show()
+
+
+# Dump pickle file of the model
+print("[INFO] Saving model...")
+pickle.dump(model,open('plant_disease_classification_model.pkl', 'wb'))
+
+
+
+# Dump pickle file of the labels
+print("[INFO] Saving label transform...")
+filename = 'plant_disease_label_transform.pkl'
+image_labels = pickle.load(open(filename, 'rb'))
+
+
+
+def predict_disease(image_path):
+    image_array = convert_image_to_array(image_path)
+    np_image = np.array(image_array, dtype=np.float16) / 225.0
+    np_image = np.expand_dims(np_image,0)
+    plt.imshow(plt.imread(image_path))
+    result = model.predict_classes(np_image)
+    print((image_labels.classes_[result][0]))
+
+
+
+    predict_disease('/content/PlantVillage/val/Potato___Early_blight/03b0d3c1-b5b0-48f4-98aa-f8904670290f___RS_Early.B 7051.JPG')
+
+
+    predict_disease('/content/PlantVillage/val/Tomato___Target_Spot/1006b3dd-22d8-41b8-b83d-08bf189fcdaa___Com.G_TgS_FL 8118.JPG')
+
+
+    predict_disease('/content/PlantVillage/val/Orange___Haunglongbing_(Citrus_greening)/02459e0c-a189-4dc9-a0dc-0548e36d0efb___CREC_HLB 5714.JPG')
+    # Download the trained model file based on its file ID.
+file_id = '1E5jNzpM__7z67GRl1cbhHK71yKcPa8wl'
+!gdown https://drive.google.com/uc?id={file_id}
+
+# Download the labels file based on its file ID.
+file_id = '1WsgEd3TG33Vj_9AAAT_WfJe_AqsuC9uu'
+!gdown https://drive.google.com/uc?id={file_id}
